b14484a251ba00a895e4c0da792c9b22197d9ce0
kalle (jag) <kalle.jillheden@gmail.com>
2018-10-18 13:56:32 +0200

    Added calculator class with basic input

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -12,17 +12,36 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Calculator.Objects;
 
 namespace Calculator
 {
+    /// <inheritdoc cref="Window" />
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly TheCalculator _calculator;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _calculator = new TheCalculator(DigitWindow.Text.Length);
+            _calculator.InputChanged += CalculatorOnInputChanged;
+        }
+
+        private void CalculatorOnInputChanged(object sender, EventArgs e)
+        {
+            DigitWindow.Text = _calculator.ToString();
+        }
+
+        private void DigitButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is Button button)) return;
+            
+            _calculator.AppendDigit(int.Parse((string) button.Content));
         }
     }
 }
---
+++ Calculator/Objects/TheCalculator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Documents;
+
+namespace Calculator.Objects
+{
+    public class TheCalculator
+    {
+        private long? _input;
+
+        public long? Input {
+            get => _input;
+            private set {
+                if (_input == value) return;
+                _input = value;
+                OnInputChanged();
+            }
+        }
+
+        public int NumOfDigits { get; }
+        public long ValueLimit { get; }
+        public long AppendLimit { get; }
+        public char PaddingChar { get; set; } = '0';
+
+        public event EventHandler InputChanged;
+
+        public TheCalculator(int numOfDigits)
+        {
+            NumOfDigits = numOfDigits;
+            // 4 digits => 10^4 - 1 => 9999
+            ValueLimit = (long)Math.Pow(10, NumOfDigits) - 1;
+            AppendLimit = (long)Math.Pow(10, NumOfDigits - 1) - 1;
+        }
+
+        public void AppendDigit(int digit)
+        {
+            if (!Input.HasValue)
+            {
+                Input = digit;
+            }
+            else if (Input <= AppendLimit)
+            {
+                Input = Input * 10 + digit;
+            }
+        }
+
+        public void Reset()
+        {
+            Input = null;
+        }
+
+        public override string ToString()
+        {
+            return (Input?.ToString() ?? PaddingChar.ToString())
+                .PadLeft(NumOfDigits, PaddingChar);
+        }
+
+        protected virtual void OnInputChanged()
+        {
+            InputChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
